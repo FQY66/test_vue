539c2a1efa5b9fa1cfda58274bb109a912fbbbc2
FQY <935947429@qq.com>
2024-12-13 11:12:56 +0800

    上传相关依赖

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -61,6 +61,7 @@
               <el-breadcrumb-item :to="{ path: '/' }">活动管理</el-breadcrumb-item>
               <el-breadcrumb-item :to="{ path: '/' }">活动列表</el-breadcrumb-item>
               <el-breadcrumb-item :to="{ path: '/' }">活动详情</el-breadcrumb-item>
+
               <el-breadcrumb-item ></el-breadcrumb-item>
             </el-breadcrumb>
 
